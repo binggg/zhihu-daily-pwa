85869076f7d5074c997d12986b0b2603b7364949
赵兵 <zhaobing@xunlei.com>
2017-02-04 16:45:46 +0800

    Add no referrer

--- server.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-request('https://news-at.zhihu.com/api/4/start-image/1080*1920', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
-  }
-})
-
